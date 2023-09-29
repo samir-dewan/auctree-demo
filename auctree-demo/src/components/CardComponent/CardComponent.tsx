7d427e683851377f3b4736d1811eb2ff7d8cd2c7
Samir Dewan <samirdewan1995@gmail.com>
2023-09-29 12:58:50 +0100

    first commit

---
+++ auctree-demo/src/components/CardComponent/CardComponent.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const CardComponent = () => {
+    return (
+        <div>
+            <Typography variant="h2">hello, world</Typography>
+        </div>
+    )
+};
+
+export default CardComponent;
---
+++ auctree-demo/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Header = () => {
+    return (
+        <div>
+            <Typography variant="h2">hello, world</Typography>
+        </div>
+    )
+};
+
+export default Header;

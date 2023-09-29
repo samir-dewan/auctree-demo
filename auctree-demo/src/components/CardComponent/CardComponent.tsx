01f2d0794ecfa520bb22c23f4757e1e0c2e1740d
Samir Dewan <samirdewan1995@gmail.com>
2023-09-29 19:55:59 +0100

    image fix

--- auctree-demo/src/components/CardComponent/CardComponent.tsx
+++ auctree-demo/src/components/CardComponent/CardComponent.tsx
@@ -1,15 +1,23 @@
-import { Typography, Container } from "@mui/material";
+import { Typography, Container, Box } from "@mui/material";
+import "./CardComponent.css";
 
 interface CardType {
   title: string,
+  subtitle: string,
+  imageUrl: string,
 }
+
 const CardComponent = (card: CardType) => {
   return (
-    <>
-      <Container>
-        <Typography variant="h2">{card.title}</Typography>
+    <div className="cardContainer">
+      <Container sx={{display: "flex", flexDirection: "column"}}>
+        <Typography variant="h1" gutterBottom>{card.title}</Typography>
+        <Typography variant="h4">{card.subtitle}</Typography>
       </Container>
-    </>
+      <Box>
+        <img src={card.imageUrl} alt={card.title} />
+      </Box>
+    </div>
   );
 };
 
